40669e3f6f70f3d2730cf0d8183a90742eca3571
Brian <brian@quantrocket.com>
2017-02-21 12:01:35 -0500

    add CLI stub for dividend service

---
+++ quantrocket/cli/subcommands/dividend.py
@@ -0,0 +1,82 @@
+# Copyright 2017 QuantRocket - All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from quantrocket.cli.utils.parse import parse_date, parse_datetime, parse_date_offset, parse_dict
+
+def add_subparser(subparsers):
+    _parser = subparsers.add_parser("dividend", description="QuantRocket dividend CLI", help="quantrocket dividend -h")
+    _subparsers = _parser.add_subparsers(title="subcommands", dest="subcommand")
+    _subparsers.required = True
+
+    parser = _subparsers.add_parser("get", help="query dividends from the dividend database")
+    parser.add_argument("-s", "--start-date", dest="start_date", metavar="DATE", help="limit to dividends on or after this ex date")
+    parser.add_argument("-e", "--end-date", dest="end_date", metavar="DATE", help="limit to dividends on or before this ex date")
+    parser.add_argument("-g", "--groups", nargs="*", metavar="GROUP", help="limit to these groups")
+    parser.add_argument("-i", "--conids", nargs="*", metavar="CONID", help="limit to these conids")
+    parser.add_argument("--exclude-groups", nargs="*", metavar="GROUP", help="exclude these groups")
+    parser.add_argument("--exclude-conids", nargs="*", metavar="CONID", help="exclude these conids")
+    parser.set_defaults(func="quantrocket.dividends.get_dividends")
+
+    parser = _subparsers.add_parser("download", help="download dividends from one or more vendors and optionally clean dividend clusters")
+    parser.add_argument("-v", "--vendors", nargs="*", choices=["reuters", "ib", "yahoo"], metavar="VENDOR", help="download dividends from these vendors")
+    parser.add_argument("-g", "--groups", nargs="*", metavar="GROUP", help="limit to these groups")
+    parser.add_argument("-i", "--conids", nargs="*", metavar="CONID", help="limit to these conids")
+    parser.add_argument("-s", "--start-date", dest="start_date", metavar="DATE", help="download dividends beginning with this ex date (applies to Yahoo only)")
+    parser.add_argument("-w", "--webhook", metavar="URL", help="post to this webhook when the downloads are complete")
+    group = parser.add_argument_group("dividend cluster cleaning options")
+    group.add_argument("-x", "--proximity", default=20, type=int, metavar="DAYS", help="consider identical dividend amounts fewer than this many days apart to be duplicates")
+    group.add_argument("-f", "--fuzziness", default=0.05, type=float, metavar="FLOAT", help="consider clustered dividend amounts differing by less than this percentage to be duplicates")
+    group.add_argument("--lookahead", dest="end_date", type=parse_date_offset, metavar="DAYS", help="limit cluster cleaning to dividends with ex dates less than this many days from today")
+    group.add_argument("--purge-minorities", dest="purge_minorities", action="store_true", help="purge clustered dividends that disagree with the majority opinion of the cluster")
+    group.add_argument("--purge-vendors", dest="purge_vendors", type=parse_dict, nargs="*", metavar="PREF:UNPREF", help="purge clustered dividends by favoring a preferred over an unpreferred vendor")
+    group.add_argument("--purge-duplicates", dest="purge_duplicates", action="store_true", help="purge clustered dividends by removing all but the first clustered dividend on a per-vendor basis")
+    parser.set_defaults(func="quantrocket.dividends.download_dividends")
+
+    parser = _subparsers.add_parser("reuters", help="download dividends from Reuters, via IB")
+    parser.add_argument("-g", "--groups", nargs="*", metavar="GROUP", help="limit to these groups")
+    parser.add_argument("-i", "--conids", nargs="*", metavar="CONID", help="limit to these conids")
+    parser.set_defaults(func="quantrocket.dividends.download_reuters_dividends")
+
+    parser = _subparsers.add_parser("yahoo", help="download dividends from Yahoo")
+    parser.add_argument("-g", "--groups", nargs="*", metavar="GROUP", help="limit to these groups")
+    parser.add_argument("-i", "--conids", nargs="*", metavar="CONID", help="limit to these conids")
+    parser.add_argument("-s", "--start-date", required=True, dest="start_date", metavar="DATE", help="download dividends beginning with this ex date")
+    parser.set_defaults(func="quantrocket.dividends.download_yahoo_dividends")
+
+    parser = _subparsers.add_parser("ib", help="download dividends from IB")
+    parser.add_argument("-g", "--groups", nargs="*", metavar="GROUP", help="limit to these groups")
+    parser.add_argument("-i", "--conids", nargs="*", metavar="CONID", help="limit to these conids")
+    parser.set_defaults(func="quantrocket.dividends.download_ib_dividends")
+
+    parser = _subparsers.add_parser("cluster", help="find and/or dividend clusters")
+    parser.add_argument("-g", "--groups", nargs="*", metavar="GROUP", help="limit to these groups")
+    parser.add_argument("-i", "--conids", nargs="*", metavar="CONID", help="limit to these conids")
+    parser.add_argument("-x", "--proximity", default=20, type=int, metavar="DAYS", help="consider identical dividend amounts fewer than this many days apart to be duplicates")
+    parser.add_argument("-f", "--fuzziness", default=0.05, type=float, metavar="FLOAT", help="consider clustered dividend amounts differing by less than this percentage to be duplicates")
+    parser.add_argument("-s", "--start-date", dest="start_date", type=parse_date, help="limit to dividends on or after this ex date")
+    parser.add_argument("--lookahead", dest="end_date", type=parse_date_offset, metavar="DAYS", help="limit to dividends with ex dates less than this many days from today")
+    parser.add_argument("--lookbehind", dest="start_date", type=parse_date_offset, metavar="DAYS", help="limit to dividends with ex dates greater than this many days from today (use negative number for past)")
+    parser.add_argument("-p", "--purge-minorities", dest="purge_minorities", action="store_true", help="purge clustered dividends that disagree with the majority opinion of the cluster")
+    parser.add_argument("-v", "--purge-vendors", dest="purge_vendors", type=parse_dict, nargs="*", metavar="PREF:UNPREF", help="purge clustered dividends by favoring a preferred over an unpreferred vendor")
+    parser.add_argument("-r", "--purge-duplicates", dest="purge_duplicates", action="store_true", help="purge clustered dividends by removing all but the first clustered dividend on a per-vendor basis")
+    parser.add_argument("-d", "--dry-run", dest="dry_run", action="store_true", default=False, help="show what would be purged but don't purge it")
+    parser.set_defaults(func="quantrocket.dividend.find_or_purge_clusters")
+
+    parser = _subparsers.add_parser("purgestale", help="purge stale dividends")
+    parser.add_argument("-b", "--created-before", dest="created_before", required=True, metavar="DATETIME", type=parse_datetime, help="purge dividends created before this datetime")
+    parser.add_argument("-a", "--exdate-after", dest="exdate_after", metavar="DATE", help="limit to dividends falling on or after this ex date")
+    parser.add_argument("-g", "--groups", nargs="*", metavar="GROUP", help="limit to these groups")
+    parser.add_argument("-i", "--conids", nargs="*", metavar="CONID", help="limit to these conids")
+    parser.add_argument("-v", "--vendors", nargs="*", choices=["reuters", "ib", "yahoo"], metavar="VENDOR", help="limit to these vendors")
+    parser.set_defaults(func="quantrocket.dividend.purge_stale_dividends")
--- quantrocket/cli/utils/parse.py
+++ quantrocket/cli/utils/parse.py
@@ -16,3 +16,44 @@ import dateutil.parser
 
 def parse_date(datestr):
     return dateutil.parser.parse(datestr).date()
+
+def parse_datetime(datetimestr):
+    return dateutil.parser.parse(datetimestr)
+
+def parse_date_offset(offset):
+    """
+    Calculates a date with the specified offset from today. Positive offsets
+    are in the future, negative offsets are in the past.
+    """
+    return datetime.date.today() + datetime.timedelta(days=int(offset))
+
+def list_or_int_or_float_or_str(value):
+    """
+    Parses the value as an int, else a float, else a string. If the value
+    contains commas, treats it as a list of ints or floats or strings.
+    """
+    if "," in value:
+        return [list_or_int_or_float_or_str(item) for item in value.split(",")]
+
+    special_vals = {
+        "None": None,
+        "True": True,
+        "False": False
+    }
+    if value in special_vals:
+        return special_vals[value]
+
+    try:
+        return int(value)
+    except ValueError:
+        try:
+            return float(value)
+        except ValueError:
+            return str(value)
+
+def parse_dict(dict_str):
+    """
+    Parses a key:value string into a dict.
+    """
+    k, v = dict_str.split(":", 1)
+    return {k:list_or_int_or_float_or_str(v)}
