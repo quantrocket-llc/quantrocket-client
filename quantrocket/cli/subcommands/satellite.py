cb10481fc4d01b14eea8aebe21a3642c78578579
Brian <brian@quantrocket.com>
2017-09-04 10:06:57 -0400

    add satellite service endpointadd zipline tearsheet endpoint

---
+++ quantrocket/cli/subcommands/satellite.py
@@ -0,0 +1,53 @@
+# Copyright 2017 QuantRocket - All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+
+def add_subparser(subparsers):
+    _parser = subparsers.add_parser("satellite", description="QuantRocket Satellite CLI", help="quantrocket satellite -h")
+    _subparsers = _parser.add_subparsers(title="subcommands", dest="subcommand")
+    _subparsers.required = True
+
+    examples = """
+Execute an abitrary command on a satellite service and optionally return a file.
+
+Examples:
+
+Run a backtrader backtest and save the performance chart to file:
+
+    quantrocket satellite exec satellite-bt 'python /etc/opt/quantrocket/quickstart.py' --return-file '/tmp/backtrader-plot.pdf' --outfile 'backtrader-plot.pdf'
+    """
+    parser = _subparsers.add_parser(
+        "exec",
+        help="execute an abitrary command on a satellite service and optionally return a file",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        "service",
+        metavar="SERVICE_NAME",
+        help="the service name")
+    parser.add_argument(
+        "cmd",
+        metavar="CMD",
+        help="the command to run")
+    parser.add_argument(
+        "-r", "--return-file",
+        metavar="FILEPATH",
+        help="the path of a file to be returned after the command completes")
+    parser.add_argument(
+        "-o", "--outfile",
+        metavar="FILEPATH",
+        dest="filepath_or_buffer",
+        help="the location to write the return_file (omit to write to stdout)")
+    parser.set_defaults(func="quantrocket.satellite._cli_execute_command")
---
+++ quantrocket/satellite.py
@@ -0,0 +1,66 @@
+# Copyright 2017 QuantRocket - All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+from quantrocket.houston import houston
+from quantrocket.cli.utils.output import json_to_cli
+from quantrocket.cli.utils.files import write_response_to_filepath_or_buffer
+
+def execute_command(service, cmd, return_file=None, filepath_or_buffer=None):
+    """
+    Execute an abitrary command on a satellite service and optionally return a file.
+
+    Parameters
+    ----------
+    service : str, required
+        the service name
+
+    cmd: str, required
+        the command to run
+
+    return_file : str, optional
+        the path of a file to be returned after the command completes
+
+    filepath_or_buffer : str, optional
+        the location to write the return_file (omit to write to stdout)
+
+    Returns
+    -------
+    dict or None
+        None if return_file, otherwise status message
+    """
+    params = {}
+    if not service:
+        raise ValueError("a service is required")
+    if not cmd:
+        raise ValueError("a command is required")
+    params["cmd"] = cmd
+    if return_file:
+        params["return_file"] = return_file
+
+    if not service.startswith("satellite"):
+        raise ValueError("service must start with 'satellite'")
+
+    response = houston.post("/{0}/commands".format(service), params=params, timeout=60*60*24)
+
+    houston.raise_for_status_with_json(response)
+
+    if return_file:
+        filepath_or_buffer = filepath_or_buffer or sys.stdout
+        write_response_to_filepath_or_buffer(filepath_or_buffer, response)
+    else:
+        return response.json()
+
+def _cli_execute_command(*args, **kwargs):
+    return json_to_cli(execute_command, *args, **kwargs)
