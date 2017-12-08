427b89e5fae060e9f63d2455f272e22ce1e1a263
Brian <brian@quantrocket.com>
2017-12-08 15:04:02 -0500

    add account balance endpoint

--- quantrocket/account.py
+++ quantrocket/account.py
@@ -12,105 +12,94 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
 from quantrocket.houston import houston
-from quantrocket.constants.account import ACCOUNT_FIELDS
+from quantrocket.cli.utils.output import json_to_cli
+from quantrocket.cli.utils.files import write_response_to_filepath_or_buffer
+from quantrocket.cli.utils.parse import dict_strs_to_dict, dict_to_dict_strs
 
-def get_balance(nlv=None, cushion=None, fields=None, accounts=None,
-                below_cushion=None, save=False):
+def download_account_balances(filepath_or_buffer=None, output="csv",
+                              start_date=None, end_date=None,
+                              latest=False, accounts=None, below=None,
+                              fields=None, no_cache=False):
     """
-    Returns a snapshot of current account balance info.
+    Query IB account balances.
 
     Parameters
     ----------
-    nlv : bool
-        only return NLV (equivalent to `fields=['NetLiquidation']`)
+    filepath_or_buffer : str or file-like object
+        filepath to write the data to, or file-like object (defaults to stdout)
 
-    cushion : bool
-        only return margin cushion (equivalent to `fields=['Cushion']`)
+    output : str
+        output format (json, csv, txt, default is csv)
 
-    fields : list, optional
-        only return the specified fields (default all fields). Possible choices:
-        AccountType, NetLiquidation, TotalCashValue, SettledCash, AccruedCash, BuyingPower,
-        EquityWithLoanValue, PreviousEquityWithLoanValue, GrossPositionValue, RegTEquity,
-        RegTMargin, SMA, InitMarginReq, MaintMarginReq, AvailableFunds, ExcessLiquidity,
-        Cushion, FullInitMarginReq, FullMaintMarginReq, FullAvailableFunds, FullExcessLiquidity,
-        LookAheadNextChange, LookAheadInitMarginReq, LookAheadMaintMarginReq, LookAheadAvailableFunds,
-        LookAheadExcessLiquidity, HighestSeverity, DayTradesRemaining, Leverage
+    start_date : str (YYYY-MM-DD), optional
+        limit to account balance snapshots taken on or after this date
 
-    accounts : list, optional
-        only return info for the specified accounts (default all connected accounts);
-        account IDs or nicknames can be provided
-
-    below_cushion : float, optional
-        only return accounts where the cushion is below this level (e.g. 0.05)
-
-    save : bool
-        save a snapshot of account balance info to the account database
-
-    Returns
-    -------
-    DataFrame
-    """
-    raise NotImplementedError("This service is not yet available")
-    response = houston.get("/account/balance")
-    response.raise_for_status()
-    return response.text
-
-def get_balance_history(start_date=None, end_date=None, nlv=None, fields=None,
-                        accounts=None, latest=False):
-    """
-    Returns historical account balance snapshots from the account database.
-
-    Parameters
-    ----------
-    start_date : date or str
-        limit to history on or after this date
-
-    end_date : date or str
-        limit to history on or before this date
-
-    nlv : bool
-        only return NLV (equivalent to `fields=['NetLiquidation']`)
-
-    fields : list, optional
-        only return the specified fields (default all fields). Possible choices:
-        AccountType, NetLiquidation, TotalCashValue, SettledCash, AccruedCash, BuyingPower,
-        EquityWithLoanValue, PreviousEquityWithLoanValue, GrossPositionValue, RegTEquity,
-        RegTMargin, SMA, InitMarginReq, MaintMarginReq, AvailableFunds, ExcessLiquidity,
-        Cushion, FullInitMarginReq, FullMaintMarginReq, FullAvailableFunds, FullExcessLiquidity,
-        LookAheadNextChange, LookAheadInitMarginReq, LookAheadMaintMarginReq, LookAheadAvailableFunds,
-        LookAheadExcessLiquidity, HighestSeverity, DayTradesRemaining, Leverage
-
-    accounts : list, optional
-        only return info for the specified accounts (default all accounts);
-        account IDs or nicknames can be provided
+    end_date : str (YYYY-MM-DD), optional
+        limit to account balance snapshots taken on or before this date
 
     latest : bool
-        only return the latest historical snapshot in the date range
+        return the latest account balance snapshot
+
+    accounts : list of str, optional
+        limit to these accounts
+
+    below : dict of FIELD:AMOUNT, optional
+        limit to accounts where the specified field is below the specified
+        amount (pass as {field:amount}, for example {'Cushion':0.05})
+
+    fields : list of str, optional
+        only return these fields (pass ['?'] or any invalid fieldname to see
+        available fields)
+
+    no_cache : bool
+        fetch account balances directly from IB (default is to query the
+        database, which is updated every minute)
 
     Returns
     -------
-    DataFrame
-    """
-    raise NotImplementedError("This service is not yet available")
-    response = houston.get("/account/balance/history")
-    response.raise_for_status()
-    return response.json()
+    None
 
-def get_portfolio(accounts=None):
-    """
-    Returns current portfolio.
+    Examples
+    --------
 
-    Parameters
-    ----------
-    accounts : list, optional
-        limit to these accounts (default all connected accounts);
-        account IDs or nicknames can be provided
-
-    Returns
-    -------
-    DataFrame
     """
-    raise NotImplementedError("This service is not yet available")
-    response = houston.get("/account/portfolio")
-    response.raise_for_status()
+    params = {}
+    if start_date:
+        params["start_date"] = start_date
+    if end_date:
+        params["end_date"] = end_date
+    if latest:
+        params["latest"] = latest
+    if accounts:
+        params["accounts"] = accounts
+    if below:
+        params["below"] = dict_to_dict_strs(below)
+    if fields:
+        params["fields"] = fields
+    if no_cache:
+        params["no_cache"] = no_cache
+
+    output = output or "csv"
+
+    if output not in ("csv", "json", "txt"):
+        raise ValueError("Invalid ouput: {0}".format(output))
+
+    response = houston.get("/account/balances.{0}".format(output), params=params)
+
+    houston.raise_for_status_with_json(response)
+
+    # Don't write a null response to file when using below filters
+    if below and response.content[:4] == b"null":
+        return
+
+    filepath_or_buffer = filepath_or_buffer or sys.stdout
+
+    write_response_to_filepath_or_buffer(filepath_or_buffer, response)
+
+def _cli_download_account_balances(*args, **kwargs):
+    below = kwargs.get("below", None)
+    if below:
+        kwargs["below"] = dict_strs_to_dict(*below)
+    return json_to_cli(download_account_balances, *args, **kwargs)
--- quantrocket/cli/subcommands/account.py
+++ quantrocket/cli/subcommands/account.py
@@ -12,33 +12,99 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from quantrocket.constants.account import ACCOUNT_FIELDS
+import argparse
+from quantrocket.cli.utils.parse import dict_str
 
 def add_subparser(subparsers):
     _parser = subparsers.add_parser("account", description="QuantRocket account CLI", help="quantrocket account -h")
     _subparsers = _parser.add_subparsers(title="subcommands", dest="subcommand")
     _subparsers.required = True
 
-    parser = _subparsers.add_parser("balance", help="get a snapshot of current account balance info")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-n", "--nlv", action="store_true", help="only show NLV")
-    group.add_argument("-c", "--cushion", action="store_true", help="only show margin cushion")
-    group.add_argument("-f", "--fields", metavar="FIELD", nargs="*", choices=ACCOUNT_FIELDS, help="only display these fields")
-    parser.add_argument("-a", "--accounts", nargs="*", metavar="ACCOUNT", help="only show these accounts")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-b", "--below-cushion", type=float, metavar="CUSHION", help="only show accounts where the cushion is below this level (e.g. 0.05)")
-    group.add_argument("-s", "--save", action="store_true", help="save a snapshot of account balance info to the account database")
-    parser.set_defaults(func="quantrocket.account.get_balance")
+    examples = """
+Query IB account balances.
 
-    parser = _subparsers.add_parser("history", help="get historical account balance snapshots from the account database")
-    parser.add_argument("-s", "--start-date", metavar="YYYY-MM-DD", help="limit to history on or after this date")
-    parser.add_argument("-e", "--end-date", metavar="YYYY-MM-DD", help="limit to history on or before this date")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-n", "--nlv", action="store_true", help="only show NLV")
-    group.add_argument("-f", "--fields", metavar="FIELD", nargs="*", choices=ACCOUNT_FIELDS, help="only display these fields")
-    parser.add_argument("-a", "--accounts", nargs="*", metavar="ACCOUNT", help="only show these accounts")
-    parser.add_argument("-l", "--latest", action="store_true", help="only show the latest historical snapshot in the date range")
-    parser.set_defaults(func="quantrocket.account.get_balance_history")
+Examples:
+
+Query the latest account balances.
+
+    quantrocket account balance --latest
+
+Query the latest NLV (Net Liquidation Value) for a particular account:
+
+    quantrocket account balance --latest --fields NetLiquidation --accounts U123456
+
+Check for accounts that have fallen below a 5% cushion and log the results,
+if any, to flightlog:
+
+    quantrocket account balance --latest --below Cushion:0.05 | quantrocket flightlog log --name quantrocket.account --level CRITICAL
+
+Query historical account balances over a date range:
+
+    quantrocket account balance --start-date 2017-06-01 --end-date 2018-01-31
+    """
+    parser = _subparsers.add_parser(
+        "balance",
+        help="query IB account balances",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    filters = parser.add_argument_group("filtering options")
+    filters.add_argument(
+        "-s", "--start-date",
+        metavar="YYYY-MM-DD",
+        help="limit to account balance snapshots taken on or after "
+        "this date")
+    filters.add_argument(
+        "-e", "--end-date",
+        metavar="YYYY-MM-DD",
+        help="limit to account balance snapshots taken on or before "
+        "this date")
+    filters.add_argument(
+        "-l", "--latest",
+        action="store_true",
+        help="return the latest account balance snapshot")
+    filters.add_argument(
+        "-a", "--accounts",
+        nargs="*",
+        metavar="ACCOUNT",
+        help="limit to these accounts")
+    filters.add_argument(
+        "-b", "--below",
+        type=dict_str,
+        nargs="*",
+        metavar="FIELD:AMOUNT",
+        help="limit to accounts where the specified field is below "
+        "the specified amount (pass as field:amount, for example Cushion:0.05)")
+    outputs = parser.add_argument_group("output options")
+    outputs.add_argument(
+        "-o", "--outfile",
+        metavar="OUTFILE",
+        dest="filepath_or_buffer",
+        help="filename to write the data to (default is stdout)")
+    output_format_group = outputs.add_mutually_exclusive_group()
+    output_format_group.add_argument(
+        "-j", "--json",
+        action="store_const",
+        const="json",
+        dest="output",
+        help="format output as JSON (default is CSV)")
+    output_format_group.add_argument(
+        "-p", "--pretty",
+        action="store_const",
+        const="txt",
+        dest="output",
+        help="format output in human-readable format (default is CSV)")
+    outputs.add_argument(
+        "-f", "--fields",
+        metavar="FIELD",
+        nargs="*",
+        help="only return these fields (pass '?' or any invalid fieldname to see "
+        "available fields)")
+    parser.add_argument(
+        "--no-cache",
+        action="store_true",
+        help="fetch account balances directly from IB (default is to query the "
+        "database, which is updated every minute)")
+    parser.set_defaults(func="quantrocket.account._cli_download_account_balances")
 
     parser = _subparsers.add_parser("portfolio", help="get current account portfolio")
     parser.add_argument("-a", "--accounts", nargs="*", metavar="ACCOUNT", help="limit to these accounts")
