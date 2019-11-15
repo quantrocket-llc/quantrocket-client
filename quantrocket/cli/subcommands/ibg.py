ecfab576c45567504995a908bf1bab0f7fd4f556
Brian <brian@quantrocket.com>
2019-11-15 11:03:00 -0500

    rename launchpad to ibg

---
+++ quantrocket/cli/subcommands/ibg.py
@@ -0,0 +1,201 @@
+# Copyright 2017 QuantRocket - All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+
+def add_subparser(subparsers):
+    _parser = subparsers.add_parser("ibg", description="QuantRocket IB Gateway service CLI", help="Start and stop IB Gateway")
+    _subparsers = _parser.add_subparsers(title="subcommands", dest="subcommand")
+    _subparsers.required = True
+
+    examples = """
+Set username/password and trading mode (paper/live) for IB Gateway, or view
+current username and trading mode.
+
+Can be used to set new credentials or switch between paper and live trading
+(must have previously entered live credentials). Setting new credentials will
+restart IB Gateway and takes a moment to complete.
+
+Examples:
+
+View current credentials for IB Gateway service named ibg1 (shows username and
+trading mode only):
+
+    quantrocket ibg credentials ibg1
+
+Set credentials for ibg1 (will prompt for password):
+
+    quantrocket ibg credentials ibg1 -u myuser --paper
+
+Leave credentials as-is but switch to live trading (must have previously entered
+live credentials):
+
+    quantrocket ibg credentials ibg1 --live
+    """
+    parser = _subparsers.add_parser(
+        "credentials",
+        help="set username/password and trading mode (paper/live) for IB Gateway",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        "gateway",
+        metavar="SERVICE_NAME",
+        help="name of IB Gateway service to set credentials for (for example, "
+        "'ibg1')")
+    parser.add_argument(
+        "-u", "--username",
+        metavar="USERNAME",
+        help="IB username (optional if only modifying trading mode)")
+    parser.add_argument(
+        "-p", "--password",
+        metavar="PASSWORD",
+        help="IB password (if omitted and user is provided, will be prompted "
+        "for password)")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "--paper",
+        action="store_const",
+        dest="trading_mode",
+        const="paper",
+        help="set trading mode to paper trading")
+    group.add_argument(
+        "--live",
+        action="store_const",
+        dest="trading_mode",
+        const="live",
+        help="set trading mode to live trading")
+    parser.set_defaults(func="quantrocket.ibg._cli_get_or_set_credentials")
+
+    examples = """
+Query statuses of IB Gateways.
+
+Examples:
+
+List the status of all gateways:
+
+    quantrocket ibg status
+
+Get a list of gateways that are running:
+
+    quantrocket ibg status --status running
+    """
+    parser = _subparsers.add_parser(
+        "status",
+        help=" query statuses of IB Gateways",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        "-s", "--status",
+        choices=["running", "stopped", "error"],
+        help="limit to IB Gateways in this status. Possible choices: %(choices)s")
+    parser.add_argument(
+        "-g", "--gateways",
+        metavar="SERVICE_NAME",
+        nargs="*",
+        help="limit to these IB Gateways")
+    parser.set_defaults(func="quantrocket.ibg._cli_list_gateway_statuses")
+
+    examples = """
+Start one or more IB Gateways.
+
+Examples:
+
+Asynchronously start all gateways (that aren't already running):
+
+    quantrocket ibg start
+
+Start specific gateways and wait for them to come up:
+
+    quantrocket ibg start --gateways ibg1 ibg3 --wait
+
+Restart all gateways:
+
+    quantrocket ibg stop --wait && quantrocket ibg start
+    """
+    parser = _subparsers.add_parser(
+        "start",
+        help="start one or more IB Gateways",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        "-g", "--gateways",
+        metavar="SERVICE_NAME",
+        nargs="*",
+        help="limit to these IB Gateways")
+    parser.add_argument(
+        "-w", "--wait",
+        action="store_true",
+        default=False,
+        help="wait for the IB Gateway  to start before returning (default is to "
+        "start the gateways asynchronously)")
+    parser.set_defaults(func="quantrocket.ibg._cli_start_gateways")
+
+    examples = """
+Stop one or more IB Gateways.
+
+Examples:
+
+Stop all gateways (that aren't already stopped):
+
+    quantrocket ibg stop
+
+Stop specific gateways and wait for them to stop:
+
+    quantrocket ibg stop --gateways ibg1 ibg3 --wait
+    """
+    parser = _subparsers.add_parser(
+        "stop",
+        help="stop one or more IB Gateways",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        "-g", "--gateways",
+        metavar="SERVICE_NAME",
+        nargs="*",
+        help="limit to these IB Gateways")
+    parser.add_argument(
+        "-w", "--wait",
+        action="store_true",
+        default=False,
+        help="wait for the IB Gateway to stop before returning (default is to stop "
+        "the gateways asynchronously)")
+    parser.set_defaults(func="quantrocket.ibg._cli_stop_gateways")
+
+    examples = """
+Upload a new IB Gateway permissions config, or return the current configuration.
+
+Permission configs are only necessary when running multiple IB Gateways with
+differing market data permissions.
+
+Examples:
+
+Upload a new config (replaces current config):
+
+    quantrocket ibg config myconfig.yml
+
+Show current config:
+
+    quantrocket ibg config
+    """
+    parser = _subparsers.add_parser(
+        "config",
+        help="upload a new config, or return the current configuration",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        "filename",
+        nargs="?",
+        metavar="FILENAME",
+        help="the config file to upload (if omitted, return the current config)")
+    parser.set_defaults(func="quantrocket.ibg._cli_load_or_show_config")
---
+++ quantrocket/ibg.py
@@ -0,0 +1,229 @@
+# Copyright 2017 QuantRocket - All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import webbrowser
+import getpass
+from quantrocket.houston import houston
+from quantrocket.exceptions import UnavailableInsideJupyter
+from quantrocket.cli.utils.output import json_to_cli
+
+def get_credentials(gateway):
+    """
+    Returns username and trading mode (paper/live) for IB Gateway.
+
+    Parameters
+    ----------
+    gateway : str, required
+        name of IB Gateway service to get credentials for (for example, 'ibg1')
+
+    Returns
+    -------
+    dict
+        credentials
+    """
+    statuses = list_gateway_statuses(gateways=[gateway])
+    if not statuses:
+        raise ValueError("no such IB Gateway: {0}".format(gateway))
+
+    response = houston.get("/{0}/credentials".format(gateway))
+    houston.raise_for_status_with_json(response)
+    # It's possible to get a 204 empty response
+    if not response.content:
+        return {}
+    return response.json()
+
+def set_credentials(gateway, username=None, password=None, trading_mode=None):
+    """
+    Set username/password and trading mode (paper/live) for IB Gateway.
+
+    Can be used to set new credentials or switch between paper and live trading
+    (must have previously entered live credentials). Setting new credentials will
+    restart IB Gateway and takes a moment to complete.
+
+    Parameters
+    ----------
+    gateway : str, required
+        name of IB Gateway service to set credentials for (for example, 'ibg1')
+
+    username : str, optional
+        IB username (optional if only modifying trading environment)
+
+    password : str, optional
+        IB password (if omitted and username is provided, will be prompted
+        for password)
+
+    trading_mode : str, optional
+        the trading mode to use ('paper' or 'live')
+
+    Returns
+    -------
+    dict
+        status message
+    """
+    statuses = list_gateway_statuses(gateways=[gateway])
+    if not statuses:
+        raise ValueError("no such IB Gateway: {0}".format(gateway))
+
+    if username and not password:
+        password = getpass.getpass(prompt="Enter IB Password: ")
+
+    data = {}
+    if username:
+        data["username"] = username
+    if password:
+        data["password"] = password
+    if trading_mode:
+        data["trading_mode"] = trading_mode
+
+    response = houston.put("/{0}/credentials".format(gateway), data=data, timeout=180)
+    houston.raise_for_status_with_json(response)
+    return response.json()
+
+def _cli_get_or_set_credentials(*args, **kwargs):
+    if kwargs.get("username", None) or kwargs.get("password", None) or kwargs.get("trading_mode", None):
+        return json_to_cli(set_credentials, *args, **kwargs)
+    else:
+        return json_to_cli(get_credentials, gateway=kwargs.get("gateway", None))
+
+def list_gateway_statuses(status=None, gateways=None):
+    """
+    Query statuses of IB Gateways.
+
+    Parameters
+    ----------
+    status : str, optional
+        limit to IB Gateways in this status. Possible choices: running, stopped, error
+
+    gateways : list of str, optional
+        limit to these IB Gateways
+
+    Returns
+    -------
+    dict of gateway:status (if status arg not provided), or list of gateways (if status arg provided)
+    """
+    params = {}
+    if gateways:
+        params["gateways"] = gateways
+    if status:
+        params["status"] = status
+
+    response = houston.get("/ibgrouter/gateways", params=params)
+    houston.raise_for_status_with_json(response)
+    return response.json()
+
+def _cli_list_gateway_statuses(*args, **kwargs):
+    return json_to_cli(list_gateway_statuses, *args, **kwargs)
+
+def start_gateways(gateways=None, wait=False):
+    """
+    Start one or more IB Gateways.
+
+    Parameters
+    ----------
+    gateways : list of str, optional
+        limit to these IB Gateways
+
+    wait: bool
+        wait for the IB Gateway to start before returning (default is to start
+        the gateways asynchronously)
+
+    Returns
+    -------
+    dict
+        status message
+    """
+    params = {"wait": wait}
+    if gateways:
+        params["gateways"] = gateways
+
+    response = houston.post("/ibgrouter/gateways", params=params, timeout=120)
+    houston.raise_for_status_with_json(response)
+    return response.json()
+
+def _cli_start_gateways(*args, **kwargs):
+    return json_to_cli(start_gateways, *args, **kwargs)
+
+def stop_gateways(gateways=None, wait=False):
+    """
+    Stop one or more IB Gateways.
+
+    Parameters
+    ----------
+    gateways : list of str, optional
+        limit to these IB Gateways
+
+    wait: bool
+        wait for the IB Gateway to stop before returning (default is to stop
+        the gateways asynchronously)
+
+    Returns
+    -------
+    dict
+        status message
+    """
+    params = {"wait": wait}
+    if gateways:
+        params["gateways"] = gateways
+
+    response = houston.delete("/ibgrouter/gateways", params=params, timeout=60)
+    houston.raise_for_status_with_json(response)
+    return response.json()
+
+def _cli_stop_gateways(*args, **kwargs):
+    return json_to_cli(stop_gateways, *args, **kwargs)
+
+def load_ibg_config(filename):
+    """
+    Upload a new IB Gateway permissions config.
+
+    Permission configs are only necessary when running multiple IB Gateways with
+    differing market data permissions.
+
+    Parameters
+    ----------
+    filename : str, required
+        the config file to upload
+
+    Returns
+    -------
+    dict
+        status message
+    """
+    with open(filename) as file:
+        response = houston.put("/ibgrouter/config", data=file.read())
+    houston.raise_for_status_with_json(response)
+    return response.json()
+
+def get_ibg_config():
+    """
+    Returns the current IB Gateway permissions config.
+
+    Returns
+    -------
+    dict
+        the config as a dict
+    """
+    response = houston.get("/ibgrouter/config")
+    houston.raise_for_status_with_json(response)
+    # It's possible to get a 204 empty response
+    if not response.content:
+        return {}
+    return response.json()
+
+def _cli_load_or_show_config(filename=None):
+    if filename:
+        return json_to_cli(load_ibg_config, filename)
+    else:
+        return json_to_cli(get_ibg_config)
