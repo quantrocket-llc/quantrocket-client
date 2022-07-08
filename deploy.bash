e42d02dfcd1c765c679a4d085dbbcdf66909dc98
Brian <brian@quantrocket.com>
2022-07-08 12:02:10 -0400

    BLD: don't trigger Docker Hub autobuild

--- deploy.bash
+++ deploy.bash
@@ -16,10 +16,3 @@ EOF
 # Deploy to pip
 python setup.py register
 python setup.py sdist bdist_wheel upload
-
-# Rebuild jupyter, countdown, satellite, moonshot Docker images with latest
-# client (and images that might run user code)
-curl -X POST https://registry.hub.docker.com/u/quantrocket/jupyter/trigger/41f6af9a-16bd-47c7-a088-71076407a7cc/
-curl -X POST https://registry.hub.docker.com/u/quantrocket/countdown/trigger/b83e185e-4702-4f41-8d4b-b7b83a24899e/
-curl -X POST https://registry.hub.docker.com/u/quantrocket/satellite/trigger/16c37457-72e1-4415-b0c4-c5185684b350/
-curl -X POST https://registry.hub.docker.com/u/quantrocket/moonshot/trigger/4a8ae8f2-f85a-4f2f-8b5d-054260e96287/
