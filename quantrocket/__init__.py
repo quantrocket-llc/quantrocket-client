05dcdd52e0b7371b483c9e4e61c7404db7f462ec
Brian <brian@quantrocket.com>
2025-10-23 17:08:28 -0400

    MAINT: update versioneer

--- quantrocket/__init__.py
+++ quantrocket/__init__.py
@@ -84,9 +84,8 @@ get_prices_reindexed_like
     databases, real-time aggregate databases, or Zipline bundles, reindexed to match
     the index (dates) and columns (sids) of the input DataFrame.
 """
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 from quantrocket import (
     account,
